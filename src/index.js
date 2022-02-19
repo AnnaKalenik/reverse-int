ce65bc110be66859b510ca81e62e05ce081a0a0d
Anna Kalenik <annakalenikalex@gmail.com>
2022-02-19 18:02:58 +0300

    reverse-int task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    const arr = n.toString().split('');
+    const reverced = [];
+
+    for (let i = arr.length; i >= 0; i--) {
+        reverced.push(arr[i]);
+    }
+
+    return parseInt(reverced.join(''));
 }
